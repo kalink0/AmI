a0fae764fdcd68dbab298d04102c12e5c8a66a3b
Marco Neumann <marco.neumann@be-binary.de>
2013-03-29 12:44:19 +0100

    First implementation fo the class ControlCenter
    Defined all required methods and attributes.

---
+++ SmartSenseo/ControlCenter/ControlCenter.py
@@ -0,0 +1,48 @@
+#SmartSenseo Control Center, Project in Ambient INtelligence, Controlling a Senseo Machine via Bluetooth
+#Copyright (C) 2013  Marco Neumann, Nils Bürkner
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class ControlCenter (object) :
+    def __init__(self) :
+        self.__status = 0
+        self.__rfid = 0
+        self.__nameofchip = '';
+        self.__numbersofchip = -1;
+        self.__btconnection = BluetoothConnection()
+
+    def __del__(self) :
+        #TODO Write into file
+        #TODO Close Bluetooth connection
+
+    def getStatus (self) :
+        #TODO Call getStatusByte from btconnection
+        return self.status
+
+    def fillCup (self, size) :
+        #TODO call sendControlBYte and call getStatusBYte
+        #TODO Exceptionhandling?
+        #TODO return?
+
+    def getRFIDNumberFromMachine (self) :
+        #TODO call getRFID
+
+    def searchUsedRFID (self) :
+        #TODO Read from File
+
+    def assignNameToRFID (self, name) :
+        self.nameofchip = name
+
+    def switchStatus (self) :
+        #TODO call sendControlByte
