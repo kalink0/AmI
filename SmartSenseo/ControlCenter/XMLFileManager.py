6aa667b4b1a9229b6b7dff3008c3c5c3599bf3d2
Marco Neumann <marco.neumann@be-binary.de>
2013-03-30 12:39:48 +0100

    Realization for writing into file
    writeToFile realized and example xml file created. Reading and writing
    should work with this xml.

--- SmartSenseo/ControlCenter/XMLFileManager.py
+++ SmartSenseo/ControlCenter/XMLFileManager.py
@@ -15,6 +15,7 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from xml.etree.ElementTree import ElementTree
+import xml.etree.ElementTree as xmltree
 
 class XMLFileManager(object):
     def __init__ (self) :
@@ -23,7 +24,26 @@ class XMLFileManager(object):
     """Method to overwrite the values of exisiting chips or append a new chip with values"""
     def writeToFile (self, rfid, nameofchip, numberofchip) :
         doc = ElementTree(file = self.__filename);
-        
+        root = doc.getroot()
+        for chips in root.getchildren():
+            chipid = int((chips.find('chip-id')).text)
+            if chipid == rfid :  #if id is already in the xml, delete the tuple
+                root.remove(chips)
+        #create the new Element for the ElementTree
+        chip = xmltree.Element('chips')
+        chip_id = xmltree.Element('chip-id')
+        chip_id.text = ElementTree.__str__(rfid)
+        name = xmltree.Element('name')
+        name.text = nameofchip
+        numbers = xmltree.Element('numbers')
+        numbers.text = ElementTree.__str__(numberofchip)
+        chip.insert(0, chip_id)
+        chip.insert(1, name)
+        chip.insert(2, numbers)
+        # and append it to the existing tree
+        root.append(chip)
+        xmltree.ElementTree(root).write(self.__filename)        
+
     """Method to search for the chip number, if chip number cannot be found, the returned id will be -1"""
     def readFromFile (self, rfid) :
         doc = ElementTree(file = self.__filename);
