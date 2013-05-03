4e4f6bf887175180d98ceab1fc969977cb073e9f
Marco Neumann <marco.neumann@be-binary.de>
2013-05-03 15:20:01 +0200

    Created class for SerialConnection
    Because of the used Bluetooth adapter, the data connection must be
    realized with a Serial Connection.
    The implementation of hte new class will be done as soon as possible

---
+++ SmartSenseo/SerialConnection/SerialConnection.py
@@ -0,0 +1,17 @@
+'''
+Created on 03.05.2013
+
+@author: inkognito
+'''
+
+class SerialConnection(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
